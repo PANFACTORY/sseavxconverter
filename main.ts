8659c9a3de338b75e6220dbc52e975810ea0bf22
PANFACTORY <yutatanabe510@gmail.com>
2021-10-10 15:21:36 +0900

    Remove yml

--- main.ts
+++ main.ts
@@ -106,7 +106,7 @@ const SSEAVX = (_eqn : Token[], _sseavx : string, _type : string) : string => {
         }
     }
     if (stack.length != 1) {
-        throw new Error("Error at AVX : May be invalid equation.");
+        throw new Error("Error at SSEAVX");
     }
     return stack.pop();
 }
